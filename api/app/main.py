235cff12803ec2dfe5c891533b11640f025af1a1
youichiro <cinnamon416@gmail.com>
2023-03-14 09:41:00 +0900

    add: response_model

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, FastAPI
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app import models, schemas
@@ -18,12 +19,14 @@ api_router = APIRouter(prefix="/api")
 @api_router.get(
     "/projects",
     status_code=200,
+    response_model=list[schemas.Project],
     responses={
         404: {"description": "Not found project"}
     },
 )
-def get_projects(db: Session = Depends(get_db)) -> list[schemas.Project]:
-    return db.query(models.Project).all()
+def get_projects(db: Session = Depends(get_db)):
+    query = select(models.Project)
+    return db.scalars(query).all()
 
 
 app.include_router(api_router)
--- api/app/schemas.py
+++ api/app/schemas.py
@@ -4,3 +4,6 @@ from pydantic import BaseModel, Field
 class Project(BaseModel):
     id: int = Field(example=1)
     name: str = Field(example="sample project")
+
+    class Config:
+        orm_mode = True
