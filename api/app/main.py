4316871427ea75d9a721539558f6dc54e3520ee9
youichiro <cinnamon416@gmail.com>
2023-03-14 09:44:00 +0900

    add: fastapi_pagination

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, FastAPI
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from fastapi_pagination import Page, add_pagination
+from fastapi_pagination.ext.sqlalchemy_future import paginate
 
 from app import models, schemas
 from app.dependencies import get_db
@@ -19,14 +21,15 @@ api_router = APIRouter(prefix="/api")
 @api_router.get(
     "/projects",
     status_code=200,
-    response_model=list[schemas.Project],
+    response_model=Page[schemas.Project],
     responses={
         404: {"description": "Not found project"}
     },
 )
 def get_projects(db: Session = Depends(get_db)):
     query = select(models.Project)
-    return db.scalars(query).all()
+    return paginate(db, query)
 
 
 app.include_router(api_router)
+add_pagination(app)
